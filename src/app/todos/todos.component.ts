86b9be848714dca0496ca8e97de7e59a5031ade2
ipavka <ipavka@ya.ru>
2022-09-19 15:28:25 +0300

    les 12.5

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,46 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Todo, TodosService } from '../services/todos.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ins-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss'],
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit, OnDestroy {
   todos: Todo[] = [];
   inputValue = '';
   error = '';
 
+  subscriptions: Subscription = new Subscription();
+
   constructor(private todoService: TodosService) {}
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.getTodos();
   }
 
   getTodos() {
-    this.todoService.getTodos().subscribe({
-      next: (res: Todo[]) => {
-        this.todos = res;
-      },
-      error: (err: HttpErrorResponse) => {
-        console.log(err);
-        this.error = err.name;
-      },
-    });
+    this.subscriptions.add(
+      this.todoService.getTodos().subscribe({
+        next: (res: Todo[]) => {
+          this.todos = res;
+        },
+        error: (err: HttpErrorResponse) => {
+          console.log(err);
+          this.error = err.name;
+        },
+      })
+    );
   }
 
   addTodoHandler() {
-    this.todoService.createTodo(this.inputValue).subscribe(res => {
-      this.todos.unshift(res.data.item);
-    });
+    this.subscriptions.add(
+      this.todoService.createTodo(this.inputValue).subscribe(res => {
+        this.todos.unshift(res.data.item);
+      })
+    );
     this.inputValue = '';
   }
 
   deleteHandler(todoId: string) {
-    this.todoService.deleteHandler(todoId).subscribe(res => {
-      console.log(res);
-      this.todos = this.todos.filter(el => el.id !== todoId);
-    });
+    this.subscriptions.add(
+      this.todoService.deleteHandler(todoId).subscribe(res => {
+        console.log(res);
+        this.todos = this.todos.filter(el => el.id !== todoId);
+      })
+    );
   }
 }
